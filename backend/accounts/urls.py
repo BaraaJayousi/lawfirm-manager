ab96fe5a9799b1f362f9f26289d3e7e10849c6a0
Baraa Jayousi <b.jayousi98@gmail.com>
2024-04-23 06:56:01 +0300

    created a view to test authentication endpoint for GET request

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('verify-email/', views.VerifyUserEmail.as_view(), name='verify_email'),
-    path('login/', views.LoginUserView.as_view(), name='login')
+    path('login/', views.LoginUserView.as_view(), name='login'),
+    path('profile/', views.TestAuthenticationView.as_view(), name='profile_test')
 
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import GenericAPIView
 from .serializers import UserRegisterSerializer, LoginSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from .utils import send_code_to_user
 from .models import OneTimePassword
 # Create your views here.
@@ -58,3 +59,14 @@ class LoginUserView(GenericAPIView):
     serializer = self.serializer_class(data=request.data, context={'request':request})
     serializer.is_valid(raise_exception=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+  
+
+class TestAuthenticationView(GenericAPIView):
+  permission_classes=[IsAuthenticated]
+  def get(self,request):
+    data={
+      'msg':'it works'
+    }
+    return Response(data, status=status.HTTP_200_OK)
+  
+  
