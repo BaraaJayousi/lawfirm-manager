03378a7bd665965dd030c95d3ef1bc3a0c484553
Baraa Jayousi <b.jayousi98@gmail.com>
2024-04-24 14:38:10 +0300

    reconfigured email settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -151,13 +151,15 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST= 'live.smtp.mailtrap.io'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST= 'live.smtp.mailtrap.io'
+# EMAIL_HOST_USER=env("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD=env("EMAIL_HOST_PASSWORD")
+EMAIL_FROM_EMAIL='mailtrap@demomailtrap.com'
+EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
 EMAIL_HOST_USER=env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD=env("EMAIL_HOST_PASSWORD")
-EMAIL_FROM_EMAIL='mailtrap@demomailtrap.com'
-
-EMAIL_PORT = '587'
+EMAIL_PORT = '2525'
 EMAIL_USE_TLS=True
 EMAIL_USE_SSL=False
 
